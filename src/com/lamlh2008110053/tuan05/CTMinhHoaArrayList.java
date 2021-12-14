ff240e5dd0e6a8e0c695e4a7f94cdaa445cdafc5
Your Name <hoailam03999@gmail.com>
2021-12-14 22:13:02 +0700

    up len moi

--- src/com/lamlh2008110053/tuan05/CTMinhHoaArrayList.java
+++ src/com/lamlh2008110053/tuan05/CTMinhHoaArrayList.java
@@ -7,11 +7,63 @@ public class CTMinhHoaArrayList
 {
     public static void main(String[] args) 
     {
+        // ArrayList Khong Co Dinh Kieu
         ArrayList a_ArrayList = new ArrayList();//ArrayList<>() Xoa <> Di Roi
-        List b_List = new ArrayList<>();//ArrayList<>() Xoa <> Di Roi
-        b_List.add("Hoai Lam");
-        b_List.add(2.9);
-        b_List.add(56);
+
+        List b_List = new ArrayList();//ArrayList<>() Xoa <> Di Roi
+
+        b_List.add("Hoai Lam");//Phan Tu 0
+        b_List.add(2.9);//Phan Tu 1
+        b_List.add(56);//Phan Tu 2
+        b_List.add(true);//Phan Tu 3
+
         System.out.println(b_List);
-    }    
+        System.out.println("-------------------------------------------------------------------------------------");
+
+        System.out.println(b_List.toString());
+        System.out.println("-------------------------------------------------------------------------------------");
+
+        Integer x = (Integer)b_List.get(2);// Ham get(int index) Lay Ra Tung Phan Tu 2
+        Boolean y = (Boolean)b_List.get(3);// Ham get(int index) Lay Ra Tung Phan Tu 3
+        String f = (String)b_List.get(0);// Ham get(int index) Lay Ra Tung Phan Tu 0
+
+        System.out.println(b_List.get(0));// Ham get(int index) Lay Ra Tung Phan Tu 0, In Ra Phan Tu 0  
+        System.out.println(x);// In Ra Phan Tu Thu 2
+        System.out.println(y);// In Ra Phan Tu Thu 3
+        System.out.println(f);// In Ra Phan Tu Thu 0
+        System.out.println("-------------------------------------------------------------------------------------");
+
+        b_List.add(new ThucPham("Nem Chua", 52.0666));//Phan Tu 4
+
+        System.out.println(b_List);
+        System.out.println("-------------------------------------------------------------------------------------");
+       
+        ThucPham thucPham = (ThucPham)b_List.get(4);// Ep Kieu
+        System.out.println("Ten ThucPham: "+ thucPham.tenThucPham + " --- "/*Them Chu public vao truoc String tenThucPham ben class ThucPham */ 
+        +"Gia Tien ThucPham: "+ thucPham.giaTienThucPham/*Them Chu public vao truoc String giaTienThucPham ben class ThucPham */);
+        System.out.println("-------------------------------------------------------------------------------------");
+        
+        // ArrayList Co Dinh Kieu La ThucPham
+        ArrayList<ThucPham> arrayListThucPham = new ArrayList<ThucPham>();
+        arrayListThucPham.add(new ThucPham("Ca Chua", 520000));
+        arrayListThucPham.add(new ThucPham("Tao", 1200000));
+        arrayListThucPham.add(new ThucPham("Ca Rot", 1600000));
+        
+        ThucPham thucPham222 = arrayListThucPham.get(0);
+        System.out.println("Ten Thuc Pham: " + thucPham222.tenThucPham + "---" + "Gia Tien Thuc Pham: " + thucPham222.giaTienThucPham);
+        thucPham222 = arrayListThucPham.get(1);
+        System.out.println("Ten Thuc Pham: " + thucPham222.tenThucPham + "---" + "Gia Tien Thuc Pham: " + thucPham222.giaTienThucPham);
+        thucPham222 = arrayListThucPham.get(2);
+        System.out.println("Ten Thuc Pham: " + thucPham222.tenThucPham + "---" + "Gia Tien Thuc Pham: " + thucPham222.giaTienThucPham);
+        System.out.println("-------------------------------------------------------------------------------------");
+        for (ThucPham thucPham2 : arrayListThucPham) 
+        {
+            thucPham2.inThongTinthucPham();    
+        }
+
+        
+    }   
+
+
+
 }
--- src/com/lamlh2008110053/tuan05/ThucPham.java
+++ src/com/lamlh2008110053/tuan05/ThucPham.java
@@ -2,21 +2,21 @@ package com.lamlh2008110053.tuan05;
 
 public class ThucPham 
 {
-    String tenThucPham;
-    double giaTienThucPham;
+    public String tenThucPham;// Them public
+    public double giaTienThucPham;// Them public
 
     ThucPham()
     {
 
     }
 
-    ThucPham(String tThucPham, double gTienThucPham)
+    public ThucPham(String tThucPham, double gTienThucPham)// Them public
     {
         tenThucPham = tThucPham;
         giaTienThucPham = gTienThucPham;
     }
     
-    void inThongTinthucPham()
+    public void inThongTinthucPham()
     {
         System.out.println("Ten Thuc Pham: "+ tenThucPham + "--" + " Gia Tien Thuc Pham: "+ giaTienThucPham);
       
